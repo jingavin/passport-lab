919052f5c68b57c0941046ce46b2277e9e530a6e
Gavin Jin <gavinjin62@gmail.com>
2025-02-07 20:14:59 -0800

    add types probably wrong i dont know what im doing

--- middleware/passportStrategies/localStrategy.ts
+++ middleware/passportStrategies/localStrategy.ts
@@ -4,7 +4,7 @@ import {
   getUserByEmailIdAndPassword,
   getUserById,
 } from "../../controllers/userController";
-import { PassportStrategy } from "../../interfaces/index";
+import { PassportStrategy, CustomUser } from "../../interfaces/index";
 
 const localStrategy = new LocalStrategy(
   {
@@ -24,7 +24,11 @@ const localStrategy = new LocalStrategy(
 /*
 FIX ME (types) 😭
 */
-passport.serializeUser(function (user: any, done: any) {
+passport.serializeUser(function (
+  user: CustomUser,
+  done: (err: any, id?: number) => void
+) {
+  console.log(JSON.stringify(user) + "SERIAL USDER");
   done(null, user.id);
 });
 
@@ -34,7 +38,10 @@ FIX ME (types) 😭
 // for done create type called user with {id:num, email: string, password: string}
 // 2 interface with the same name will be mergged together, called delcaration merging
 // done looks for user interface which is empty on defualt
-passport.deserializeUser(function (id: number, done: any) {
+passport.deserializeUser(function (
+  id: number,
+  done: (err: any, id?: CustomUser | null) => void
+) {
   let user = getUserById(id);
   if (user) {
     done(null, user);
